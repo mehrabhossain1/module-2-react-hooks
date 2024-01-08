adc584d6128527e3b32717cb4f85c70e1ab09c47
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-01-08 19:06:50 +0600

    object as state

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react'
 
 const Form = () => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
+  //   const [name, setName] = useState('')
+  //   const [email, setEmail] = useState('')
+
+  const [user, setUser] = useState({ name: '', email: '' })
 
   const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault()
-    console.log({ name, email })
+    console.log(user)
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <input
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) => setUser({ ...user, name: e.target.value })}
         type='text'
         name='name'
         id='name'
       />
       <input
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
         type='text'
         name='email'
         id='email'
