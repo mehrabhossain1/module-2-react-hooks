7a50e039256667be22171704502df85c414a9086
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-01-10 19:45:48 +0600

    done module-2

--- src/pages/UseEffectExample.tsx
+++ src/pages/UseEffectExample.tsx
@@ -2,23 +2,47 @@ import { useEffect, useState } from 'react'
 
 const UseEffectExample = () => {
   const [hidden, setHidden] = useState(false)
+  const [user, setUser] = useState({ name: '', email: '' })
+
+  console.log(user)
 
   useEffect(() => {
     console.log('Render')
-
-    return () => {
-      console.log('Inside cleanup')
-    }
-  }, [hidden])
+  }, [user.name, user.email])
 
   return (
     <div>
-      <button onClick={() => setHidden((prev) => !prev)}>
-        {hidden ? 'Show' : 'Hide'}
-      </button>
-      {hidden && <Todo />}
+      <input
+        onBlur={(e) => setUser({ ...user, name: e.target.value })}
+        type='text'
+        name='name'
+        id='name'
+      />
+      <input
+        onBlur={(e) => setUser({ ...user, email: e.target.value })}
+        type='text'
+        name='email'
+        id='email'
+      />
     </div>
   )
+
+  // useEffect(() => {
+  //   console.log('Render')
+
+  //   return () => {
+  //     console.log('Inside cleanup')
+  //   }
+  // }, [hidden])
+
+  // return (
+  //   <div>
+  //     <button onClick={() => setHidden((prev) => !prev)}>
+  //       {hidden ? 'Show' : 'Hide'}
+  //     </button>
+  //     {!hidden && <Todo />}
+  //   </div>
+  // )
 }
 
 const Counter = () => {
@@ -46,6 +70,10 @@ const Todo = () => {
     fetch('https://jsonplaceholder.typicode.com/todos/1', { signal })
       .then((res) => res.json())
       .then((data) => alert(data.title))
+
+    return () => {
+      controller.abort()
+    }
   }, [])
   return <div>Todo</div>
 }
