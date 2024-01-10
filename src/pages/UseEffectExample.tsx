089e100b8f538b86f5c26be5a85cc1e9688750f9
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-01-10 16:19:21 +0600

    clean  up

--- src/pages/UseEffectExample.tsx
+++ src/pages/UseEffectExample.tsx
@@ -3,17 +3,27 @@ import { useEffect, useState } from 'react'
 const UseEffectExample = () => {
   const [hidden, setHidden] = useState(false)
 
-  useEffect(() => {
-    console.log('Render')
+  const [count, setCount] = useState(0)
 
-    return () => {
-      console.log('Inside cleanup')
-    }
-  }, [hidden])
+  //   useEffect(() => {
+  //     console.log('Render')
+
+  //     return () => {
+  //       console.log('Inside cleanup')
+  //     }
+  //   }, [hidden])
+
+  useEffect(() => {
+    setInterval(() => {
+      setCount((prev) => prev + 1)
+      console.log('setInterval')
+    }, 1000)
+  }, [])
 
   return (
     <div>
-      <button onClick={() => setHidden((prev) => !prev)}>Click</button>
+      {/* <button onClick={() => setHidden((prev) => !prev)}>Click</button> */}
+      <h1>{count}</h1>
     </div>
   )
 }
